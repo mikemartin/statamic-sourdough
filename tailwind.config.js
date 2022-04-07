7796085b9995060c99ecb3010611890e498ec274
Mike Martin <hello@mike-martin.ca>
2022-04-07 16:24:47 +1000

    Style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     './resources/**/*.antlers.html',
@@ -5,10 +7,31 @@ module.exports = {
     './resources/**/*.vue',
     './content/**/*.md'
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ['Sansita', 'sans-serif'],
+        sans: ['Archivo', 'sans-serif'],
+      },
+      colors: {
+        primary: colors.orange,
+        secondary: colors.amber,
+        success: colors.green,
+        error: colors.red,
+        neutral: colors.stone,
+        social: {
+          twitter: '#1DA1F2',
+          linkedin: '#1a91da',
+          instagram: '#e1306c',
+          facebook: '#3b5998',
+          youtube: '#ff0000',
+        },
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
   ],
 }
